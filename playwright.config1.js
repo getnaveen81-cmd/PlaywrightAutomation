d084d75879a150ada3eb6e3ef843dbc8e5503dc8
Naveen kumar S <naveenkumar.s@apexon.com>
2026-08-03 11:39:40 +0530

    View port Property on emulating browser to mobile devices with playwright

--- playwright.config1.js
+++ playwright.config1.js
@@ -2,6 +2,10 @@
 /* to run tjis config file 
 use this command in terminal to run 
 -> npx playwright test ./tests/ClientAppPO.spec.js --config playwright.config1.js --project=safari */
+
+const { permission } = require("node:process");
+const { devices } = require("playwright/test");
+
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
@@ -21,16 +25,20 @@ const config = {
         trace: "on",
         screenshot: "only-on-failure",
         video: "retain-on-failure",
+        ...devices['iPhone 16 Pro Max'] // Emulates the iPhone 16 Pro Max device
       },
     },
     {
       name: "chrome",
       use: {
         browserName: "chromium",
-        headless: true,
+        headless: false,
         trace: "on",
         screenshot: "only-on-failure",
         video: "retain-on-failure",
+        ignoreHttpsErros:true, // Ignores SSL/HTTPS certificate errors
+        Permissions:['geolocation'], 
+        viewport : {width:720,height:720} // Overrides the default device viewport size
       },
     },
   ],
